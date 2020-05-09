3757aa5142e18797940ad6a908396567ce9fe29f
rednaxela700 <ohleg@windowslive.com>
2020-05-09 17:39:05 +0200

    created a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends React.Component{
+
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+    
 }
 
 export default App;
