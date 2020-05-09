78b474c11da55daa538e6d65dad5dc3959c5b7a0
rednaxela700 <ohleg@windowslive.com>
2020-05-09 18:37:56 +0200

    added tailiwind config files

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Navbar extends Component{
+    render() {
+        return (
+            <nav className={'flex p-5'}>
+                <h1>navbar</h1>
+            </nav>
+        )
+    }
+}
