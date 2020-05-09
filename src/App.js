01ce5d03889ad3df856c8883295ed7cd6a79608f
rednaxela700 <ohleg@windowslive.com>
2020-05-09 19:23:16 +0200

    added navbar with icon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, {Fragment} from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends React.Component {
-
     render() {
-        const name = 'John Doe'
+        const name = "John Doe";
         return (
-            <Fragment>
-                <div className="h-24 flex justify-end bg-blue-300">
-                    <Navbar/>
-
-                </div>
-            </Fragment>
-        )
+            <div className="h-24 flex justify-start content-center bg-blue-300">
+                <Navbar title="Github Finder" />
+            </div>
+        );
     }
-
 }
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-export default class Navbar extends Component{
+export default class Navbar extends Component {
     render() {
         return (
-            <nav className={'flex p-5'}>
-                <h1>navbar</h1>
+            <nav className="container flex items-center">
+                <div className="flex flex-auto items-center text-white text-lg ml-10">
+                    <FontAwesomeIcon icon={faGithub} className="text-4xl" />
+                    <h1 className="text-4xl px-1 mx-2 capitalize">
+                        {this.props.title}
+                    </h1>
+                </div>
             </nav>
-        )
+        );
     }
 }
