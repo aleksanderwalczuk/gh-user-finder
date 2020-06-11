6adff3eb7b87e657cee5fad09d278be6969de1f8
rednaxela700 <ohleg@windowslive.com>
2020-06-11 17:04:22 +0200

    components/layout/Navbar.js: added links, centered nav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Navbar = ({ title }) => {
     return (
-        <nav className="container flex items-center">
+        <nav className="container flex justify-between items-center mx-auto">
             <div className="flex flex-auto items-center text-white text-lg ml-10">
                 <FontAwesomeIcon icon={faGithub} className="text-4xl" />
                 <h1 className="text-4xl px-1 mx-2 capitalize">{title}</h1>
+            </div>
+            <div >
+                <ul className="flex text-white text-md">
+                    <li className='mx-2'><Link to={'/'}>Home</Link></li>
+                    <li className='mx-2'><Link to={'/about'}>About</Link></li>
+                </ul>
             </div>
         </nav>
     );
