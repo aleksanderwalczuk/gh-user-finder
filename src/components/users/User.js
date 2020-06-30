0c0411959acd59d6d801ee0b5f0528afd2a73354
rednaxela700 <ohleg@windowslive.com>
2020-06-30 19:18:46 +0200

    components/users/User.js: added basic UI for user view

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
-import React, {Component} from "react";
+import React, { Component, Fragment } from "react";
+import PropTypes from 'prop-types'
+import Loader from "react-spinners/CircleLoader";
+import { Link } from "react-router-dom";
 
 export default class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login)
         console.log(this.props.getUser(this.props.match.params.login))
+    }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
     }
 
     render() {
@@ -13,9 +22,52 @@ export default class User extends Component {
             url,
             location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable
         } = this.props.user
-        const {loading} = this.props
-        return (<div>{name}</div>
+        const { loading } = this.props
+        if (loading) {
+            return <Loader />
+        }
+        return (
+            <Fragment>
+                <Link
+                    to='/'
+                    className='button px-2 py-1 m-2 bg-teal-400  rounded text-white'
+                >
+                    Back to Search
+                </Link>
+                Hireable: {' '}
+                {
+                    hireable ? (
+                        <span>YES!</span>
+                    ) : (
+                            <span>NO.</span>
+                        )
+                }
+                <div>{name}</div>
+                <div className='card center'>
+                    <div className=''>
+                        <img
+                            src={avatar}
+                            style={{ width: '150px' }}
+                        />
+                    </div>
+                    <h2>{name}</h2>
+                    <p>Location: {location}</p>
+                    <div>{bio && <Fragment>
+                        <h3>Bio</h3>
+                        <p>
+                            {bio}
+                        </p>
+                    </Fragment>}
+                    </div>
+                    <a
+                    href={html_url}
+                    className='button px-2 py-1 m-2 bg-teal-400  rounded text-white'
+                    >
+                        Link to Github
+                        </a>
 
+                </div>
+            </Fragment>
         )
     }
 
